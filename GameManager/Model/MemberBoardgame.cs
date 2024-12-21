5a4d89b8e968107e68be30ebc9f31056dd879122
llavner <marcus.renvall@gmail.com>
2024-12-21 12:35:52 +0100

    Added to model

---
+++ GameManager/Model/MemberBoardgame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager.Model
+{
+    public class MemberBoardgame
+    {
+
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+        public int BoardgameId { get; set; }
+        public Boardgame Boardgame { get; set; }
+
+    }
+}
